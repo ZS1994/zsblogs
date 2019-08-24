140401cdaa468d9db9e12eab0d5dc64972bcc86a
ZS1994 <1217360619@qq.com>
2019-08-24 23:20:01 +0800

    基金历史爬虫机器人2号只能爬取前面几个基金问题已经解决，原因在于有的基金是货币基金数据结构不同导致解析数据时错位，由于数据类型不对而插入失败，。又由于整体是一个try导致后续代码都不走了。
    Signed-off-by: ZS1994 <1217360619@qq.com>

--- zsblogs/src/main/java/com/zs/tools/CrawlerNo2.java
+++ zsblogs/src/main/java/com/zs/tools/CrawlerNo2.java
@@ -75,35 +75,45 @@ public class CrawlerNo2 implements Runnable{
 		while(true){
 			try {
 				if (isBegin) {
-					try {
-						//张顺，2019-7-14，1，设置一些配置参数，这个网址内容已经改了，它不能一次查出所有，所以只能根据分页循环取值
-						int pageRows=20;//张顺，2019-7-14，每页多少数据量
-						//张顺，2019-7-14，-1
-						EasyUIAccept accept=new EasyUIAccept();
-						accept.setStart(0);
-						accept.setRows(Constans.INFINITY);
-						List<FundInfo> fis=fundInfoMapper.queryFenye(accept);
-						for (FundInfo fi : fis) {
-							//找到该基金的当日净值
-							String url="http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code="+fi.getId()+"&page=1&per="+pageRows+"&sdate=&edate=";
-							String str=HttpClientReq.httpGet(url, null,null);
-							str=str.replaceFirst("var apidata=", "");
-							str=str.substring(0, str.length()-1);
-							JSONObject jsonObject=JSONObject.parseObject(str);
-							int pageSize=jsonObject.getIntValue("pages");
-							//张顺，2019-7-14，2，因为不能一次取值，所以必须循环遍历所有页面
-							if (pageSize>1) {
-								for (int pageNo = 1; pageNo <= pageSize; pageNo++) {
+					//张顺，2019-7-14，1，设置一些配置参数，这个网址内容已经改了，它不能一次查出所有，所以只能根据分页循环取值
+					int pageRows=20;//张顺，2019-7-14，每页多少数据量
+					//张顺，2019-7-14，-1
+					EasyUIAccept accept=new EasyUIAccept();
+					accept.setStart(0);
+					accept.setRows(Constans.INFINITY);
+					List<FundInfo> fis=fundInfoMapper.queryFenye(accept);
+					for (FundInfo fi : fis) {
+						log.info("【基金编号】"+fi.getId());
+						//找到该基金的当日净值
+						//modify begin 1 张顺 2019年8月24日 <不要当获取出错时导致整个后续的都不走了>
+						String url="http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code="+fi.getId()+"&page=1&per="+pageRows+"&sdate=&edate=";;
+						String str="";
+						try {
+							str=HttpClientReq.httpGet(url, null,null);
+						} catch (Exception e) {
+							//跳过改基金，继续走下一个基金
+							continue;
+						}
+						//modify end -1 张顺 2019年8月24日 <描述>
+						str=str.replaceFirst("var apidata=", "");
+						str=str.substring(0, str.length()-1);
+						JSONObject jsonObject=JSONObject.parseObject(str);
+						int pageSize=jsonObject.getIntValue("pages");
+						//张顺，2019-7-14，2，因为不能一次取值，所以必须循环遍历所有页面
+						if (pageSize>1) {
+							for (int pageNo = 1; pageNo <= pageSize; pageNo++) {
+								try {
 									//不想浪费资源，这里判断一下如果连续累计找到5个已存在的(返回的是false)，那么久不往下找了，认为下面都是已存在的，即以前已经获取过了
 									if (loopSave(pageNo, pageRows, fi.getId())==false) {
+										log.info("【不往下找基金历史了】"+fi.getName()+"("+fi.getId()+")");
 										break;
-									};
+									}
+								} catch (Exception e) {
+									log.error(e.toString());
 								}
 							}
-							//张顺，2019-7-14，-2
 						}
-					} catch (Exception e) {
-						e.printStackTrace();
+						//张顺，2019-7-14，-2
 					}
 					Thread.sleep(1000*60*60*2);//每2小时重新爬取一次
 				}
@@ -126,12 +136,19 @@ public class CrawlerNo2 implements Runnable{
 	 * @throws Exception 
 	 */
 	@SuppressWarnings("unused")
-	private boolean loopSave(int pageNo,int pageRows,String code) throws Exception{
+	private boolean loopSave(int pageNo,int pageRows,String code){
 		//已存在的记录数，超过10次就不找了，返回false
 		int exits=0;
 		//找到该基金的当日净值
 		String url="http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code="+code+"&page="+pageNo+"&per="+pageRows+"&sdate=&edate=";
-		String str=HttpClientReq.httpGet(url, null,null);
+		String str=null;
+		try {
+			str = HttpClientReq.httpGet(url, null,null);
+		} catch (Exception e1) {
+			e1.printStackTrace();
+			log.error("【网络错误：无法获取访问基金历史网站】"+e1.toString());
+			return false;
+		}
 		str=str.replaceFirst("var apidata=", "");
 		str=str.substring(0, str.length()-1);
 		JSONObject jsonObject=JSONObject.parseObject(str);
@@ -147,7 +164,13 @@ public class CrawlerNo2 implements Runnable{
 			Elements summaryR=doc.select("tr:eq("+i+") td:eq(3)");
 			
 			Double nv=summaryE.html().trim().equals("")?0.00:Double.valueOf(summaryE.html());
-			Date d=summaryD.html().trim().equals("")?sdf.parse("2018-1-2"):sdf.parse(summaryD.html());
+			Date d=null;
+			try {
+				d = summaryD.html().trim().equals("")?sdf.parse("2018-1-2"):sdf.parse(summaryD.html());
+			} catch (ParseException e1) {
+				e1.printStackTrace();
+				log.error("【日期错误："+summaryD.html()+"】");
+			}
 			Double rate=summaryR.html().trim().equals("")?0.00:Double.valueOf(summaryR.html().replaceAll("%", ""));
 			
 			//尝试插入
@@ -162,7 +185,7 @@ public class CrawlerNo2 implements Runnable{
 				timelineMapper.insert(tl);
 			} catch (Exception e) {
 				exits++;//已存在了，累加
-				log.info("【不必关注】张顺，2019-7-14，（这个代表插入失败，因为基金历史表设置了为唯一索引fi_id_2，目的是防止重复插入。）"+e.getMessage());
+				log.info("【不必关注】张顺，基金编号："+history.getFiId()+"（这个代表插入失败，因为基金历史表设置了为唯一索引fi_id_2，目的是防止重复插入。）"+e.getMessage());
 			}
 		}
 		return true;
@@ -190,7 +213,57 @@ public class CrawlerNo2 implements Runnable{
 
 
 
-	
+	public static void main(String[] args) {
+		/*while(true){
+			try {
+				if (true) {
+					try {
+						//张顺，2019-7-14，1，设置一些配置参数，这个网址内容已经改了，它不能一次查出所有，所以只能根据分页循环取值
+						int pageRows=20;//张顺，2019-7-14，每页多少数据量
+						//张顺，2019-7-14，-1
+						EasyUIAccept accept=new EasyUIAccept();
+						accept.setStart(0);
+						accept.setRows(Constans.INFINITY);
+						List<FundInfo> fis=fundInfoMapper.queryFenye(accept);
+						for (FundInfo fi : fis) {
+							//找到该基金的当日净值
+							String url="http://fund.eastmoney.com/f10/F10DataApi.aspx?type=lsjz&code="+fi.getId()+"&page=1&per="+pageRows+"&sdate=&edate=";
+							String str=HttpClientReq.httpGet(url, null,null);
+							str=str.replaceFirst("var apidata=", "");
+							str=str.substring(0, str.length()-1);
+							JSONObject jsonObject=JSONObject.parseObject(str);
+							int pageSize=jsonObject.getIntValue("pages");
+							//张顺，2019-7-14，2，因为不能一次取值，所以必须循环遍历所有页面
+							if (pageSize>1) {
+								for (int pageNo = 1; pageNo <= pageSize; pageNo++) {
+									//不想浪费资源，这里判断一下如果连续累计找到5个已存在的(返回的是false)，那么久不往下找了，认为下面都是已存在的，即以前已经获取过了
+									if (loopSave(pageNo, pageRows, fi.getId())==false) {
+										log.info("【不往下找基金历史了了】"+fi.getName()+"("+fi.getId()+")");
+										break;
+									};
+								}
+							}
+							//张顺，2019-7-14，-2
+						}
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					Thread.sleep(1000*60*60*2);//每2小时重新爬取一次
+				}
+				Thread.sleep(1000*3);//每3s进行一次判断
+			} catch (Exception e) {
+				//出错了就休息2小时再尝试
+				e.printStackTrace();
+				try {
+					Thread.sleep(1000*60*60*2);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+			}
+		}*/
+		
+		
+	}
 	
 	
 	
